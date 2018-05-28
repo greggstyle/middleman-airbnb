8809da791c615d3fe36849737b1e11580a507911
gregory hanffou <ghanffou@gmail.com>
2018-05-28 17:14:22 +0200

    almost finish

--- config.rb
+++ config.rb
@@ -20,3 +20,17 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+# ["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+#   proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+# end
+
+data.users.each do |name|
+  proxy "/flats/#{name.github_nickname}.html", "/flats/show.html", locals: { owner: name.github_nickname }, ignore: true
+end
+
+# proxy "/flats/papillard.html", "/flats/show.html", locals: { owner: "papillard" }, ignore: true
+
+# whenever the user goes to /flats/papillard.html (in the browser)
+# Middleman will use /flats/show.html.erb as a template
+# Middleman will create a local variable called owner which is equal to papillard
